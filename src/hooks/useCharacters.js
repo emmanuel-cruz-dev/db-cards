c2c7f21628ca904c6635d3df3bac91baa02de3f0
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-25 16:27:15 -0300

    feat: adds useGetCharacters and useCharacterById to src/hooks/useCharacters.js

---
+++ src/hooks/useCharacters.js
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { characterService } from "../services/characters";
+
+export const useGetCharacters = () => {
+  return useQuery({
+    queryKey: ["characters"],
+    queryFn: characterService.getCharacters,
+  });
+};
+
+export const useCharacterById = (id) => {
+  return useQuery({
+    queryKey: ["character", id],
+    queryFn: characterService.getCharacterById(id),
+    enabled: !!id,
+  });
+};
