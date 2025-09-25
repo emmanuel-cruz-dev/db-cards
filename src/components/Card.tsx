6301dc0b7e8eeb6e1b6174590a132a78817687c4
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-25 09:51:29 -0300

    feat: updates styles and structure in src/components/Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-function Card() {
+function Card({ name, description, image }) {
   return (
     <div className="card" style={{ width: "18rem" }}>
-      <img src="..." className="card-img-top" alt="..." />
+      <img
+        src={image}
+        className="card-img-top img-fluid"
+        style={{ height: "14rem", objectFit: "contain" }}
+        alt={name}
+      />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text text-truncate">{description}</p>
         <a href="#" className="btn btn-primary">
           Go somewhere
         </a>
