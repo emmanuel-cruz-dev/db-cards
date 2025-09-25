d7b7e5e37123311edcd0d191243fa2fce710b0f7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-25 13:12:32 -0300

    feat: adds AppRouter.jsx to src/routes/

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavigationBar from "../layouts/NavigationBar";
+import Footer from "../layouts/Footer";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
+import Planets from "../pages/Planets";
+import Transformations from "../pages/Transformations";
+// import Details from "../pages/Details";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+        <NavigationBar />
+        <main className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/planets" element={<Planets />} />
+            <Route path="/transformations" element={<Transformations />} />
+            {/* <Route path="/details/:id" element={<Details />} /> */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default AppRouter;
