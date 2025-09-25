d3f4b4d8edd6917ff4106ef7a39cc20bf7fb98de
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-25 16:26:32 -0300

    feat: adds getCharacters and getCharacterById to src/services/characters.js

---
+++ src/services/characters.js
@@ -0,0 +1,29 @@
+import axios from "./axiosConfig";
+
+const getCharacters = async () => {
+  try {
+    const response = await axios.get("/api/characters");
+    return response.data;
+  } catch (error) {
+    const axiosError = error;
+    console.error(
+      "[getCharacters] Error fetching characters:",
+      axiosError.response.data
+    );
+  }
+};
+
+const getCharacterById = async (id) => {
+  try {
+    const response = await axios.get(`/api/characters/${id}`);
+    return response.data;
+  } catch (error) {
+    const axiosError = error;
+    console.error(
+      "[getCharacterById] Error fetching character:",
+      axiosError.response.data
+    );
+  }
+};
+
+export const characterService = { getCharacters, getCharacterById };
