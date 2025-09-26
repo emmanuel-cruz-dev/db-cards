cd5d1d12b91ded4f01e6257b677f3fc16265ed5c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-26 11:33:52 -0300

    style: modifies padding and font-weight in src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import CharactersGallery from "../components/ui/CharactersGallery";
 function Home() {
   return (
     <Container className="py-4">
-      <h1 className="display-4 fw-normal">Personajes</h1>
+      <h1 className="display-4 fw-semibold pb-4">Personajes</h1>
       <CharactersGallery />
     </Container>
   );
