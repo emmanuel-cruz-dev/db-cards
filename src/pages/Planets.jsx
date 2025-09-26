38f52d40b03c8847ef5045b76539761aa172e0a5
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-26 12:21:56 -0300

    feat: adds PlanetsGallery and updates styles in src/pages/Planets.jsx

--- src/pages/Planets.jsx
+++ src/pages/Planets.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import PlanetsGallery from "../components/ui/PlanetsGallery";
 
 function Planets() {
-  return <Container>Planetas</Container>;
+  return (
+    <Container className="py-4">
+      <h1 className="display-5 fw-semibold pb-4 text-light">Planetas</h1>
+      <PlanetsGallery />
+    </Container>
+  );
 }
 
 export default Planets;
