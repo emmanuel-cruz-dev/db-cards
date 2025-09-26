f0601b2ba3b53cbb259e3dc17db09da064163cf7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-26 12:22:46 -0300

    style: adds display-5 and text-light to h1 tag in src/pages/Transformations.jsx

--- src/pages/Transformations.jsx
+++ src/pages/Transformations.jsx
@@ -5,7 +5,9 @@ import TransformationsGallery from "../components/ui/TransformationsGallery";
 function Transformations() {
   return (
     <Container className="py-4">
-      <h1 className="display-4 fw-semibold pb-4">Transformaciones</h1>
+      <h1 className="display-5 fw-semibold pb-4 text-light">
+        Transformaciones
+      </h1>
       <TransformationsGallery />
     </Container>
   );
