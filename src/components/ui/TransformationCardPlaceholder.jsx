c34e1760a1956a678a51e50b51c8c25455a9ca4f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-27 14:20:34 -0300

    feat: adds TransformationCardPlaceholder.jsx to src/components/ui/

---
+++ src/components/ui/TransformationCardPlaceholder.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, Placeholder } from "react-bootstrap";
+
+function TransformationCardPlaceholder() {
+  return (
+    <Card style={{ width: "15rem" }}>
+      <Placeholder
+        as="div"
+        animation="glow"
+        style={{ height: "14rem", backgroundColor: "#e9ecef" }}
+      />
+      <Card.Body>
+        <Placeholder as={Card.Title} animation="glow">
+          <Placeholder xs={6} />
+        </Placeholder>
+        <Placeholder as={Card.Text} animation="glow">
+          <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />
+        </Placeholder>
+        <Placeholder.Button variant="primary" xs={6} animation="glow" />
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default TransformationCardPlaceholder;
