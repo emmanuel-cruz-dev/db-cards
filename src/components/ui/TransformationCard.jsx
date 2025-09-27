b9e00a409ce96d30418ef8416c3273ec383cc84d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-27 14:22:37 -0300

    feat: adds to TransformationCardPlaceholder src/components/ui/TransformationCard.jsx

--- src/components/ui/TransformationCard.jsx
+++ src/components/ui/TransformationCard.jsx
@@ -1,29 +1,10 @@
 import React from "react";
-import { Card, Placeholder } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import TransformationCardPlaceholder from "./TransformationCardPlaceholder";
 
-function TransformationCard({ id, name, image, description, ki, isLoading }) {
-  if (isLoading) {
-    return (
-      <Card style={{ width: "15rem" }}>
-        <Placeholder
-          as="div"
-          animation="glow"
-          style={{ height: "14rem", backgroundColor: "#e9ecef" }}
-        />
-        <Card.Body>
-          <Placeholder as={Card.Title} animation="glow">
-            <Placeholder xs={6} />
-          </Placeholder>
-          <Placeholder as={Card.Text} animation="glow">
-            <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{" "}
-            <Placeholder xs={6} /> <Placeholder xs={8} />
-          </Placeholder>
-          <Placeholder.Button variant="primary" xs={6} animation="glow" />
-        </Card.Body>
-      </Card>
-    );
-  }
+function TransformationCard({ id, name, image, ki, isLoading }) {
+  if (isLoading) return <TransformationCardPlaceholder />;
 
   return (
     <Card style={{ width: "15rem" }}>
@@ -40,11 +21,10 @@ function TransformationCard({ id, name, image, description, ki, isLoading }) {
       />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        <Card.Text className="mb-1">
+        <Card.Text className="mb-3">
           <strong>Ki:</strong> {ki}
         </Card.Text>
-        <Card.Text className="line-clamp-3">{description}</Card.Text>
-        <Link to={`/details/${id}`} className="btn btn-primary">
+        <Link to={`/transformations/${id}`} className="btn btn-primary">
           Ver más
         </Link>
       </Card.Body>
