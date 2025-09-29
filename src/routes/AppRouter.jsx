6879eefdca9d79db7011be74d85bb3141a4dc952
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-29 12:22:55 -0300

    feat: removes NavitationBar and Footer from src/routes/AppRouter.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,7 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
-import NavigationBar from "../layouts/NavigationBar";
-import Footer from "../layouts/Footer";
 import LoadingFallback from "../components/ui/LoadingFallback";
 
 const Home = lazy(() => import("../pages/Home"));
@@ -16,28 +14,20 @@ const TransformationDetails = lazy(() =>
 
 const AppRouter = () => {
   return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <NavigationBar />
-        <main className="flex-grow-1 bg-dark">
-          <Suspense fallback={<LoadingFallback />}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/characters/:id" element={<CharacterDetails />} />
-              <Route path="/planets" element={<Planets />} />
-              <Route path="/planets/:id" element={<PlanetDetails />} />
-              <Route path="/transformations" element={<Transformations />} />
-              <Route
-                path="/transformations/:id"
-                element={<TransformationDetails />}
-              />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Suspense>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <Suspense fallback={<LoadingFallback />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/characters/:id" element={<CharacterDetails />} />
+        <Route path="/planets" element={<Planets />} />
+        <Route path="/planets/:id" element={<PlanetDetails />} />
+        <Route path="/transformations" element={<Transformations />} />
+        <Route
+          path="/transformations/:id"
+          element={<TransformationDetails />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 };
 
