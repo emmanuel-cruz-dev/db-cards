6dcebf660315c30e9ece07ddd1e0c13168268fc6
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-29 09:49:03 -0300

    feat: adds Suspense and lazy loading to src/routes/AppRouter.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import NavigationBar from "../layouts/NavigationBar";
 import Footer from "../layouts/Footer";
-import Home from "../pages/Home";
-import NotFound from "../pages/NotFound";
-import Planets from "../pages/Planets";
-import Transformations from "../pages/Transformations";
-import PlanetDetails from "../pages/PlanetDetails";
-import CharacterDetails from "../pages/CharacterDetails";
-import TransformationDetails from "../pages/TransformationDetails";
+import LoadingFallback from "../components/ui/LoadingFallback";
+
+const Home = lazy(() => import("../pages/Home"));
+const NotFound = lazy(() => import("../pages/NotFound"));
+const Planets = lazy(() => import("../pages/Planets"));
+const Transformations = lazy(() => import("../pages/Transformations"));
+const PlanetDetails = lazy(() => import("../pages/PlanetDetails"));
+const CharacterDetails = lazy(() => import("../pages/CharacterDetails"));
+const TransformationDetails = lazy(() =>
+  import("../pages/TransformationDetails")
+);
 
 const AppRouter = () => {
   return (
@@ -15,18 +20,20 @@ const AppRouter = () => {
       <div className="d-flex flex-column min-vh-100">
         <NavigationBar />
         <main className="flex-grow-1 bg-dark">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/characters/:id" element={<CharacterDetails />} />
-            <Route path="/planets" element={<Planets />} />
-            <Route path="/planets/:id" element={<PlanetDetails />} />
-            <Route path="/transformations" element={<Transformations />} />
-            <Route
-              path="/transformations/:id"
-              element={<TransformationDetails />}
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Suspense fallback={<LoadingFallback />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/characters/:id" element={<CharacterDetails />} />
+              <Route path="/planets" element={<Planets />} />
+              <Route path="/planets/:id" element={<PlanetDetails />} />
+              <Route path="/transformations" element={<Transformations />} />
+              <Route
+                path="/transformations/:id"
+                element={<TransformationDetails />}
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </main>
         <Footer />
       </div>
