04e13decf1048ebeff394f37262fc12deeb8b95b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-29 09:49:47 -0300

    feat: adds usePagination to src/components/ui/PlanetsGallery.jsx

--- src/components/ui/PlanetsGallery.jsx
+++ src/components/ui/PlanetsGallery.jsx
@@ -2,33 +2,68 @@ import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import PlanetCard from "./PlanetCard";
 import { useGetPlanets } from "../../hooks/usePlanets";
+import PaginationItem from "./PaginationItem";
+import { usePagination } from "../../hooks/usePagination";
 
 function PlanetsGallery() {
-  const { data: planets, isLoading, error } = useGetPlanets();
+  const { currentPage, handlePageChange } = usePagination(1);
+  const { data: planets, isLoading, error } = useGetPlanets(currentPage);
 
   if (error) return <p>Error al cargar los planetas: {error.message}</p>;
 
+  if (isLoading && !planets) {
+    return (
+      <Container>
+        <article>
+          <p>Cargando páginas...</p>
+        </article>
+        <Row className="g-3 justify-content-center">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <Col key={`placeholder-${index}`} xs={12} md={7} lg={6} xl={4}>
+              <PlanetCard isLoading={true} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
+
+  if (!planets || !planets.items) {
+    return <p>No se han encontrado planetas</p>;
+  }
+
   return (
     <Container>
+      {planets.meta && (
+        <article className="text-center text-light mb-3">
+          <p>
+            Página <strong>{planets.meta.currentPage}</strong> de{" "}
+            <strong>{planets.meta.totalPages}</strong>
+          </p>
+        </article>
+      )}
       <Row className="g-3 justify-content-center">
-        {isLoading
-          ? Array.from({ length: 6 }).map((_, index) => (
-              <Col key={`placeholder-${index}`} xs={12} md={7} lg={6} xl={4}>
-                <PlanetCard isLoading={true} />
-              </Col>
-            ))
-          : planets.items.map((planet) => (
-              <Col key={planet.id} xs={12} md={7} lg={6} xl={4}>
-                <PlanetCard
-                  id={planet.id}
-                  name={planet.name}
-                  image={planet.image}
-                  description={planet.description}
-                  isDestroyed={planet.isDestroyed}
-                />
-              </Col>
-            ))}
+        {planets.items.map((planet) => (
+          <Col key={planet.id} xs={12} md={7} lg={6} xl={4}>
+            <PlanetCard
+              id={planet.id}
+              name={planet.name}
+              image={planet.image}
+              description={planet.description}
+              isDestroyed={planet.isDestroyed}
+            />
+          </Col>
+        ))}
       </Row>
+
+      {planets.meta && (
+        <PaginationItem
+          currentPage={planets.meta.currentPage}
+          totalPages={planets.meta.totalPages}
+          onPageChange={handlePageChange}
+          isLoading={isLoading}
+        />
+      )}
     </Container>
   );
 }
