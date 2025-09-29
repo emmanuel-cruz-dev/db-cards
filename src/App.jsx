a23665c5dbdd3658a35c451521d1a614162e05c0
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-29 12:22:14 -0300

    feat: adds NavigationBar and Footer to src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
+import NavigationBar from "./layouts/NavigationBar";
+import Footer from "./layouts/Footer";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+        <NavigationBar />
+        <main className="flex-grow-1 bg-dark">
+          <AppRouter />
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
