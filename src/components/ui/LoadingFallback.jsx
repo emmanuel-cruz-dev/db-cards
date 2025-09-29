4bc7a1e1ccd25df9fdbec0e2fcb7e54062e71ea2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-29 12:23:39 -0300

    feat: updates styles and structure in src/components/ui/LoadingFallback.jsx

--- src/components/ui/LoadingFallback.jsx
+++ src/components/ui/LoadingFallback.jsx
@@ -1,15 +1,30 @@
-const LoadingFallback = () => (
-  <section
-    className="d-flex justify-content-center align-items-center"
-    style={{ minHeight: "60vh" }}
-  >
-    <article className="text-center">
-      <main className="spinner-border text-warning mb-3" role="status">
-        <span className="visually-hidden">Cargando...</span>
-      </main>
-      <p className="text-light">Cargando página...</p>
-    </article>
-  </section>
-);
+import DBLogo from "../../assets/dragon-ball-logo.avif";
+import { pulseKeyframes, pulseStyle } from "../../constants/pulseStyles";
+
+const LoadingFallback = () => {
+  return (
+    <>
+      <style>{pulseKeyframes}</style>
+      <section
+        className="d-flex justify-content-center align-items-center"
+        style={{ minHeight: "60vh" }}
+      >
+        <article className="text-center">
+          <figure style={{ maxWidth: "400px" }}>
+            <img
+              className="img-fluid"
+              src={DBLogo}
+              alt="Logo de Dragon Ball"
+              width="900"
+              height="900"
+              style={pulseStyle}
+            />
+          </figure>
+          <p className="visually-hidden">Cargando...</p>
+        </article>
+      </section>
+    </>
+  );
+};
 
 export default LoadingFallback;
