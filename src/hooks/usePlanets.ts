fd4e835b1dd733d845b64d198b138632ef2a2042
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-14 16:46:59 -0300

    chore: convert usePlanets.js to usePlanets.ts in src/hooks/

--- src/hooks/usePlanets.ts
+++ src/hooks/usePlanets.ts
@@ -1,14 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { planetService } from "../services/planets";
 
-export const useGetPlanets = (page) => {
+export const useGetPlanets = (page: number) => {
   return useQuery({
     queryKey: ["planets", page],
     queryFn: () => planetService.getPlanets(page),
   });
 };
 
-export const usePlanetById = (id) => {
+export const usePlanetById = (id: string) => {
   return useQuery({
     queryKey: ["planet", id],
     queryFn: () => planetService.getPlanetById(id),
