7cddec499c52701afa99fbebde4b9214b16374e6
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-14 16:43:03 -0300

    chore: convert useCharacters and adds types in function props in src/hooks

--- src/hooks/useCharacters.ts
+++ src/hooks/useCharacters.ts
@@ -1,14 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { characterService } from "../services/characters";
 
-export const useGetCharacters = (page) => {
+export const useGetCharacters = (page: number) => {
   return useQuery({
     queryKey: ["characters", page],
     queryFn: () => characterService.getCharacters(page),
   });
 };
 
-export const useCharacterById = (id) => {
+export const useCharacterById = (id: string) => {
   return useQuery({
     queryKey: ["character", id],
     queryFn: () => characterService.getCharacterById(id),
