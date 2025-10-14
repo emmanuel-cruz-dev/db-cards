1d042b9a243543f7473a3da3a301e23b2bc3eb6c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-14 15:43:37 -0300

    chore: convert main.jsx to main.tsx in src/

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,7 +14,7 @@ const queryClient = new QueryClient({
   },
 });
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
