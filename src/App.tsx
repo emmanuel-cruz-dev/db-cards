050910e02bf3c993bbab3fd070ea420656b8b5b8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-10-28 17:06:14 -0300

    feat: adds ProgressBar to src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
 import NavigationBar from "./layouts/NavigationBar";
 import Footer from "./layouts/Footer";
+import ProgressBar from "./components/ui/ProgressBar";
 
 function App() {
   return (
     <Router>
+      <ProgressBar />
       <div className="d-flex flex-column min-vh-100">
         <NavigationBar />
         <main className="flex-grow-1 bg-dark">
